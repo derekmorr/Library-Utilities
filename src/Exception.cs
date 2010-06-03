c85c397478f20496e678df9a83380dcfba2d7add
asridevi <asridevi@pdx.edu>
2010-06-03 00:56:12 +0000

    Added copyright notice (closes issue #1) and license information(closes issue #2) to all the source files.  Date and author information was retrieved from the FLEL Subversion repository.

--- src/Exception.cs
+++ src/Exception.cs
@@ -1,3 +1,17 @@
+// Copyright 2004 University of Wisconsin 
+// All rights reserved. 
+// 
+// The copyright holder licenses this file under the New (3-clause) BSD 
+// License (the "License").  You may not use this file except in 
+// compliance with the License.  A copy of the License is available at 
+// 
+//   http://www.opensource.org/licenses/bsd-license.php 
+// 
+// and is included in the NOTICE.txt file distributed with this work.
+// 
+// Contributors: 
+//   James Domingo, Forest Landscape Ecology Lab, UW-Madison 
+
 namespace Edu.Wisc.Forest.Flel.Util
 {
     /// <summary>
