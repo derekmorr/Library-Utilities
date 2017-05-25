586ad2e6a2976627e79abac1d704412047b15327
Aleksandr.Karifanov <aleksandr.karifanov@effective-soft.com>
2017-05-25 11:30:02 +0300

    Added generating assembly version.
    Fixed build configuration. Changed x86 configuration on any cpu configuration.

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -3,3 +3,5 @@ using System.Reflection;
 [assembly: AssemblyDescription("Utility Library")]
 [assembly: AssemblyCompany("UW-Madison, Forest Landscape Ecology Lab")]
 [assembly: AssemblyCopyright("Copyright 2004-2005 University of Wisconsin")]
+
+[assembly: AssemblyVersion("1.0.*")]
