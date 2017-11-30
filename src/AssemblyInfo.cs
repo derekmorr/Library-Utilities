184faa9f93595bacdb467efc67969bfda53c7978
Derek Morr <derekmorr@psu.edu>
2017-11-30 11:22:19 -0500

    port to .NET Core 2.0 ; fix namespaces

--- src/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyDescription("Utility Library")]
-[assembly: AssemblyCompany("UW-Madison, Forest Landscape Ecology Lab")]
-[assembly: AssemblyCopyright("Copyright 2004-2005 University of Wisconsin")]
-
-[assembly: AssemblyVersion("1.0.*")]
--- src/SpeciesEcoregionAuxParm.cs
+++ src/SpeciesEcoregionAuxParm.cs
@@ -1,10 +1,13 @@
 ﻿using Landis.Core;
+using Landis.Library.Parameters;
+using Landis.Library.Parameters.Ecoregions;
+//using Landis.Library.Parameters.Species;
 
 namespace Landis.Utilities
 {
    public class SpeciesEcoregionAuxParm<T>
     {
-       Parameters.Species.AuxParm<Parameters.Ecoregions.AuxParm<T>> values;
+       Species.AuxParm<Landis.Library.Parameters.Ecoregions.AuxParm<T>> values;
        ///<Summary>
        /// Gets a species and ecoregion specific value
        ///</Summary>
@@ -25,10 +28,10 @@ namespace Landis.Utilities
         ///</Summary>
         public SpeciesEcoregionAuxParm(ISpeciesDataset speciesDataset, IEcoregionDataset ecoregionDataset)
         {
-            values = new Parameters.Species.AuxParm<Parameters.Ecoregions.AuxParm<T>>(speciesDataset);
+            values = new Species.AuxParm<Landis.Library.Parameters.Ecoregions.AuxParm<T>>(speciesDataset);
             foreach (ISpecies species in speciesDataset)
             {
-                values[species] = new Parameters.Ecoregions.AuxParm<T>(ecoregionDataset);
+                values[species] = new Landis.Library.Parameters.Ecoregions.AuxParm<T>(ecoregionDataset);
             }
         }
     }
